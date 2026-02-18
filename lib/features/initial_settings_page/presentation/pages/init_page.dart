9aa29427b1c08516216bd0a8b12f46e9e4ef20ef
V <guglyanskayapochta@gmail.com>
2026-02-18 12:54:42 +0300

    Добавлена анимация показа и скрытия логотипа
    На главной странице отрегулировано местоположение выпадающего списка и
    добавлены показ и скрытие логотипа в зависимости от того, открыта ли
    клавиатура.

--- lib/features/initial_settings_page/presentation/pages/init_page.dart
+++ lib/features/initial_settings_page/presentation/pages/init_page.dart
@@ -18,149 +18,165 @@ import 'package:timetable_app/shared/presentation/widgets/error_message_block.da
 import 'package:timetable_app/shared/presentation/widgets/loading_indicator_block.dart';
 
 class InitPage extends StatefulWidget {
-  const InitPage({super.key});
+	const InitPage({super.key});
 
-  @override
-  State<InitPage> createState() => _InitPageState();
+	@override
+	State<InitPage> createState() => _InitPageState();
 }
 
 class _InitPageState extends State<InitPage> {
-  final TextEditingController _groupController = TextEditingController();
-  bool _showGroupNotSelectedMsg = false;
+	final TextEditingController _groupController = TextEditingController();
+	bool _showGroupNotSelectedMsg = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.darkBackground,
-      body: BlocListener<InitPageBloc, InitPageBlocState>(
-        listener: (BuildContext context, state) {
-          if (state is InitPageSetupFinished) {
-            if (context.mounted) {
-              context.go(homePagePath);
-            }
-          }
-        },
-        child: BlocBuilder<InitPageBloc, InitPageBlocState>(
-          builder: (context, state) {
-            return switch (state) {
-              InitPageInitial() => const SizedBox.shrink(),
-              InitPageLoading() ||
-              InitPageSetupFinished() => LoadingIndicatorBlock(),
-              InitPageError(errorMessage: final msg) => ErrorMessageBlock(
-                circumstances: "загрузке списка групп",
-                errorMessage: msg,
-                onRetry: () => context.read<InitPageBloc>().add(InitEvent()),
-              ),
-              InitPageLoaded(groups: final groups) => Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(color: AppColors.primary, width: 2),
-                      ),
-                      elevation: 0,
-                      color: Colors.transparent,
-                      child: SvgPicture.asset(
-                        'assets/logo.svg',
-                        colorFilter: ColorFilter.mode(
-                          AppColors.primary,
-                          BlendMode.srcOut,
-                        ),
-                        width: 200,
-                        height: 200,
-                      ),
-                    ),
-                    const SizedBox(height: 10,),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        "Впишите название своей группы, чтобы мы могли загрузить ваше расписание",
-                        style: TextStyle(
-                          color: AppColors.textAccent,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: AutocompleteField(
-                        label: "Например: 22-КБ-ИВ1",
-                        controller: _groupController,
-                        onSelected: (value) {
-                          setState(() {
-                            _showGroupNotSelectedMsg = false;
-                          });
-                        },
-                        optionsBuilder: (value) {
-                          if (value.text.isEmpty) {
-                            return const Iterable.empty();
-                          }
-                          return groups
-                              .where(
-                                (group) => group.name.toLowerCase().contains(
-                                  value.text.toLowerCase(),
-                                ),
-                              )
-                              .map((group) => group.name)
-                              .take(20);
-                        },
-                      ),
-                    ),
-                    if (_showGroupNotSelectedMsg)
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          textAlign: TextAlign.center,
-                          "Введено неверное значение. Пожалуйста, выберите группу из списка или введите заново свою группу",
-                          style: TextStyle(
-                            color: AppColors.error,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () async {
-                        if (groups.indexWhere(
-                              (e) => e.name == _groupController.text,
-                            ) ==
-                            -1) {
-                          setState(() {
-                            _showGroupNotSelectedMsg = true;
-                          });
-                          return;
-                        }
-                        context.read<InitPageBloc>().add(
-                          SetupFinishedEvent(group: _groupController.text),
-                        );
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: AppColors.primary,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 12,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      child: Text(
-                        "Продолжить",
-                        style: TextStyle(color: AppColors.textPrimary),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            };
-          },
-        ),
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		final isKeyboardVisible = MediaQuery.of(context).viewInsets.bottom > 0;
+		return Scaffold(
+			backgroundColor: AppColors.darkBackground,
+			body: BlocListener<InitPageBloc, InitPageBlocState>(
+			listener: (BuildContext context, state) {
+				if (state is InitPageSetupFinished) {
+						if (context.mounted) {
+							context.go(homePagePath);
+						}
+					}
+			},
+			child: BlocBuilder<InitPageBloc, InitPageBlocState>(
+				builder: (context, state) {
+					return switch (state) {
+						InitPageInitial() => const SizedBox.shrink(),
+						InitPageLoading() ||
+						InitPageSetupFinished() => LoadingIndicatorBlock(),
+						InitPageError(errorMessage: final msg) => ErrorMessageBlock(
+							circumstances: "загрузке списка групп",
+							errorMessage: msg,
+							onRetry: () => context.read<InitPageBloc>().add(InitEvent()),
+						),
+						InitPageLoaded(groups: final groups) => Center(
+							child: Column(
+								mainAxisAlignment: isKeyboardVisible ? .center : .center,
+								children: [
+									// if (isKeyboardVisible)
+									// SizedBox(height: 32,),
+									Spacer(flex: 1,),
+									Flexible(
+										flex: 2,
+									  child: AnimatedContainer(
+									  	duration: !isKeyboardVisible ? Duration(milliseconds: 100) : Duration(milliseconds: 0),
+									  	curve: Curves.linear,
+									  	height: isKeyboardVisible ? 0 : 210, // Чуть больше высоты картинки для отступа
+									  	width: isKeyboardVisible ? 0 : 210, // Чуть больше высоты картинки для отступа
+									  	child: Center(
+									  		child: Card(
+									  			shape: RoundedRectangleBorder(
+									  				side: BorderSide(color: AppColors.primary, width: 2),
+									  			),
+									  			elevation: 0,
+									  			color: Colors.transparent,
+									  			child: SvgPicture.asset(
+									  				'assets/logo.svg',
+									  				colorFilter: ColorFilter.mode(
+									  					AppColors.primary,
+									  					BlendMode.srcOut,
+									  				),
+									  				width: 200,
+									  				height: 200,
+									  			),
+									  		),
+									  	),
+									  ),
+									),
+									const SizedBox(height: 10,),
+									Padding(
+										padding: const EdgeInsets.all(16.0),
+										child: Text(
+											textAlign: TextAlign.center,
+											"Впишите название своей группы, чтобы мы могли загрузить ваше расписание",
+											style: TextStyle(
+												color: AppColors.textAccent,
+												fontSize: 20,
+												fontWeight: FontWeight.bold,
+											),
+										),
+									),
+									const SizedBox(height: 10),
+									Padding(
+										padding: const EdgeInsets.symmetric(horizontal: 16.0),
+										child: AutocompleteField(
+											label: "Например: 22-КБ-ИВ1",
+											controller: _groupController,
+											onSelected: (value) {
+												setState(() {
+													_showGroupNotSelectedMsg = false;
+												});
+											},
+											optionsBuilder: (value) {
+												if (value.text.isEmpty) {
+														return const Iterable.empty();
+													}
+												return groups
+													.where(
+													(group) => group.name.toLowerCase().contains(
+															value.text.toLowerCase(),
+														),
+													)
+													.map((group) => group.name)
+													.take(20);
+											},
+										),
+									),
+									if (_showGroupNotSelectedMsg)
+									Padding(
+										padding: const EdgeInsets.all(16.0),
+										child: Text(
+											textAlign: TextAlign.center,
+											"Введено неверное значение. Пожалуйста, выберите группу из списка или введите заново свою группу",
+											style: TextStyle(
+												color: AppColors.error,
+												fontSize: 18,
+												fontWeight: FontWeight.bold,
+											),
+										),
+									),
+									const SizedBox(height: 32),
+									TextButton(
+										onPressed: () async {
+											if (groups.indexWhere(
+															(e) => e.name == _groupController.text,
+														) ==
+														-1) {
+													setState(() {
+														_showGroupNotSelectedMsg = true;
+													});
+													return;
+												}
+											context.read<InitPageBloc>().add(
+												SetupFinishedEvent(group: _groupController.text),
+											);
+										},
+										style: TextButton.styleFrom(
+											backgroundColor: AppColors.primary,
+											padding: const EdgeInsets.symmetric(
+												horizontal: 24,
+												vertical: 12,
+											),
+											shape: RoundedRectangleBorder(
+												borderRadius: BorderRadius.circular(8),
+											),
+										),
+										child: Text(
+											"Продолжить",
+											style: TextStyle(color: AppColors.textPrimary),
+										),
+									),
+									Spacer(flex: 2,),
+								],
+							),
+						),
+					};
+				},
+			),
+		),
+		);
+	}
 }
